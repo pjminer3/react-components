b442df5002d76623729f31a08bffc0078d2f1920
Patrick Miner <patrickminer318@gmail.com>
2017-10-25 11:34:53 -0700

    Dynamically creating an array of component items... I think... Because the directions are partial

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// TODO
+var Grocerylist = () => (
+<ul>
+  <GroceryListItem items={['Bread']} />
+  <GroceryListItem items={['Water']} />
+  <GroceryListItem items={['Am I doing this?!']} />
+</ul>
+);
+
+// var BreadComponent = () => (
+//   <li>Bread</li>
+// )
+
+// var WaterComponent = () => (
+//   <li>Water</li>
+// )
+
+var GroceryListItem = (props) => (
+  <li>{props.items[0]}</li>
+);
+
+ReactDOM.render(<Grocerylist />, document.getElementById('app'));
